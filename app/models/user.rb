99cc314486a2f58fc09be40c17247975da294d36
Anne-Marie Nault <anne.ma@gmail.com>
2022-04-28 15:34:47 -0400

    Add pool model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :discussions, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :discussion_subscriptions, dependent: :destroy
+  has_many :pools, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
 end
